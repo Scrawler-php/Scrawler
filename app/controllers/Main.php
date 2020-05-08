0407f33de3a27c9cb4371decb60d8e62223caaf7
physcoccode <pranjaltech10@gmail.com>
2020-05-08 22:18:08 +0530

    remove unwanted file

--- app/controllers/Main.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-Class Main {
-    function getHi(){
-        $user = s()->db()::get('user',1);
-        return s()->t()->render('hello',['user'=>$user->name]);
-    }
-}
--- app/views/hello.blade.php
+++
@@ -1,5 +0,0 @@
-<h1 >@hello($user)</h1>
-
-{{-- @if($greet)
-<p> greetings </p>
-@endif --}}
