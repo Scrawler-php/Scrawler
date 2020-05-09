4f8078e7512f9249c58a3fd930d7e0db6c50873a
physcoccode <pranjaltech10@gmail.com>
2020-05-10 00:48:35 +0530

    feat. slim mode

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-
-include __DIR__.'/vendor/autoload.php';
 include __DIR__.'/rb-mysql.php';
+include __DIR__.'/core/service/slim.php';
 
-include __DIR__.'/core/boot.php';
+/**
+ * Check for slim mode before loading framework
+ */
+if(\Scrawler\Service\Slim::isSlim()){
+     $slim = new \Scrawler\Service\Slim();
+     echo $slim->dispatch();
+}else{
+    include __DIR__.'/vendor/autoload.php';
+    include __DIR__.'/core/boot.php';
+}
+
