e7d1edd89422881d344d7b9d66943a7bbc468c40
physcoccode <pranjaltech10@gmail.com>
2020-05-13 15:23:22 +0530

    fix: broken slim mode

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-include __DIR__.'/vendor/autoload.php';
+include __DIR__.'/vendor/scrawler/framework/src/Slim.php';
+
+if(\Scrawler\Slim::isSlim()){
+    $slim = \Scrawler\Slim();
+    echo $slim->dispatch();
+}else{
+    include __DIR__.'/vendor/autoload.php';
+}
+
+
+
+
