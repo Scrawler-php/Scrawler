6b315939f78d415056cfa104d084f9c215825eb5
physcoccode <pranjaltech10@gmail.com>
2020-05-13 13:13:45 +0530

    fix: index (decoupled)

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-include __DIR__.'/rb-mysql.php';
-include __DIR__.'/core/Service/Slim.php';
-
-/**
- * Check for slim mode before loading framework
- */
-if(\Scrawler\Service\Slim::isSlim()){
-     $slim = new \Scrawler\Service\Slim();
-     echo $slim->dispatch();
-}else{
-    include __DIR__.'/vendor/autoload.php';
-    include __DIR__.'/core/Boot.php';
-}
-
+include __DIR__.'/vendor/autoload.php';
