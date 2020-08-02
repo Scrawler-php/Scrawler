89182de1f9491bf9c9fd053507cba1b83bd5789d
Pranjal Pandey <pranjaltech10@gmail.com>
2020-08-02 22:26:47 +0530

    chore:remove slim mode

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 include __DIR__.'/vendor/scrawler/framework/src/Slim.php';
+include __DIR__.'/bootstrap.php';
 
-if(\Scrawler\Slim::isSlim()){
-    $slim = \Scrawler\Slim();
-    echo $slim->dispatch();
-}else{
-    include __DIR__.'/vendor/autoload.php';
-    include __DIR__.'/bootstrap.php';
-}
 
 
 
